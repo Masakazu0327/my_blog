d59a30871ccf13c62321a9a494530d86b0b44f50
Masakazu0327 <masakazu.sato.iwt@gmail.com>
2019-05-03 10:43:52 +0900

    一覧機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
+  # 修正後の内容のみ記載
   resources :posts
   root 'posts#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
